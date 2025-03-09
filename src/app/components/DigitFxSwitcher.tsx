e0e3f76023451058f6c9d3823752da1bb05164f8
jakfils <jakfils@gmail.com>
2025-03-09 19:17:08 +0100

    increase fx digit toggle buttons widht

--- src/app/components/DigitFxSwitcher.tsx
+++ src/app/components/DigitFxSwitcher.tsx
@@ -9,7 +9,7 @@ const DigitFxSwitcher: React.FC<DigitFxSwitcherProps> = ({
   return (
     <div className="mt-5 flex  text-md sm:hidden">
       <button
-        className={`cursor-pointer rounded-l-2xl border-y border-l border-[var(--digit-bg-color)] px-4 ${!isFxActive ? "bg-[var(--digit-bg-color)]" : "bg-[var(--calculaor-bg-color)]"}`}
+        className={`cursor-pointer rounded-l-full border-y border-l border-[var(--digit-bg-color)]  py-2 px-8 ${!isFxActive ? "bg-[var(--digit-bg-color)]" : "bg-[var(--calculaor-bg-color)]"}`}
         onClick={() => {
           handleFx("123");
         }}
@@ -17,7 +17,7 @@ const DigitFxSwitcher: React.FC<DigitFxSwitcherProps> = ({
         123
       </button>
       <button
-        className={`cursor-pointer rounded-r-2xl border-y border-r border-[var(--digit-bg-color)] px-4 ${isFxActive ? "bg-[var(--digit-bg-color)]" : "bg-[var(--calculaor-bg-color)]"}`}
+        className={`cursor-pointer rounded-r-full border-y border-r border-[var(--digit-bg-color)] py-2 px-8 ${isFxActive ? "bg-[var(--digit-bg-color)]" : "bg-[var(--calculaor-bg-color)]"}`}
         onClick={() => {
           handleFx("fx");
         }}
