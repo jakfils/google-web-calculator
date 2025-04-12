d5391f557486289e42b6950f5049a0f156616173
jakfils <jakfils@gmail.com>
2025-04-12 08:41:30 +0100

    Feat: Add overflow-y in History Component

--- src/app/components/History.tsx
+++ src/app/components/History.tsx
@@ -9,13 +9,14 @@ const History: React.FC<HistoryProps> = ({
   handleIsHistoryShown,
 }) => {
   return isHistoryShown ? (
-    <div className="absolute top-0 mt-1 min-h-30 w-5/6 rounded-xl bg-[var(--calculator-bg-color)] p-4 sm:w-4/5">
+    <div className="absolute top-0 mt-1 max-h-96 min-h-30 w-5/6 overflow-y-auto rounded-xl bg-[var(--calculator-bg-color)] p-4 sm:max-h-80 sm:w-4/5">
       <MdHistory
         onClick={() => {
           handleIsHistoryShown();
         }}
       />
       <h1>History</h1>
+      <p></p>
     </div>
   ) : null;
 };
